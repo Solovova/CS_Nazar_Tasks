38bdcbc76a768c6798bb71f991d6b644e10023cf
SoloVova <vsolo0@gmail.com>
2022-04-17 13:01:52 +0300

    0.07

--- Tasks/Task02/Task02Solution.cs
+++ Tasks/Task02/Task02Solution.cs
@@ -10,9 +10,9 @@ public class Task02Solution : ITaskSolution<Task02Data>{
 
         for (int len = data.InArr1.Length; len > 0; len--){
             for (int pos1 = 0; pos1 <= data.InArr1.Length-len; pos1++){
-                var slice1 = data.InArr1[pos1..(pos1+len)];
+                var slice1 = data.InArr1.AsSpan()[pos1..(pos1+len)];
                 for (int pos2 = 0; pos2 <= data.InArr2.Length-len; pos2++){
-                    var slice2 = data.InArr2[pos2..(pos2+len)];
+                    var slice2 = data.InArr2.AsSpan()[pos2..(pos2+len)];
                     if (slice1.SequenceEqual(slice2)){
                         data.OutArr = slice1.ToArray();
                         return data;
